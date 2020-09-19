5b5f9da1163b84dcc2770737fd2e25adfc810797
Fatimo <fatimofo@buffalo.edu>
2020-09-19 16:31:30 -0400

    case 02

--- week_02/case_study_02.R
+++ week_02/case_study_02.R
@@ -1,1 +1,24 @@
- 
+library(tidyverse)
+temp=read_csv("https://data.giss.nasa.gov/tmp/gistemp/STATIONS/tmp_USW00014733_14_0_1/station.csv",
+              skip=1,
+              na="999.90", 
+              col_names = c("YEAR","JAN","FEB","MAR", # define column names 
+                            "APR","MAY","JUN","JUL",  
+                            "AUG","SEP","OCT","NOV",  
+                            "DEC","DJF","MAM","JJA",  
+                            "SON","metANN"))
+view(temp)
+summary(temp)
+glimpse(temp)
+head(temp)
+tail(glimpse)
+
+ggplot(temp, aes(YEAR, JJA))+
+  geom_line() + geom_smooth() +
+  labs(x="Year", y="Mean Summer Temp(C)",
+       title="Annual Mean Summer Temperatures in Buffalo, NY",
+       subtitle = "Summer Months include June, July, and August
+       Data from the Global Historical Climate Network
+       Blue line is a LOESS smooth")
+
+
