e7e4b82fee49cdc45b844c59572481b34916a4b0
Fatimo <fatimofo@buffalo.edu>
2020-09-19 16:19:21 -0400

    case 01

--- week_01/case_study_01.R
+++ week_01/case_study_01.R
@@ -1,1 +1,15 @@
 data(iris)
+str(iris)
+iris$Petal.Length
+summary(iris$Petal.Length)
+str(iris$Petal.Length)
+mean(iris$Petal.Length)
+library(ggplot2)
+png(file="Histogram.png")
+ggplot(iris, aes(Petal.Length))+ 
+  geom_histogram(binwidth =2.5, alpha=0.4,col="green",
+                 fill="blue", 
+                 breaks=seq(1,7,by=0.1)) + 
+  labs(title="Histogram of Petal Length")
+
+dev.off()
