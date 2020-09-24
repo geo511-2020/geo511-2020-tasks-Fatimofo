e0d57bf9bd58330cb75c748d37b4fce027b4ace4
Fatimo <fatimofo@buffalo.edu>
2020-09-24 16:47:57 -0400

    Updated case 03

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -5,8 +5,8 @@ library(gapminder)
 gapminderMinusKuwait <- gapminder %>%
   filter(!country %in% c("Kuwait"))
 #Plot1
-png(filename = "GDP Vs Life Expectancy.png", height=5, width = 15, 
-    units ="in",res = 72)
+png(filename = "Plot1.png", height=5, width = 15, 
+    units ="in", res=300)
 ggplot(gapminderMinusKuwait, aes(lifeExp, gdpPercap)) +
   geom_point(aes(size=pop/100000, color=continent)) + 
   theme_bw() + scale_y_continuous(trans="sqrt")+ 
@@ -17,24 +17,36 @@ ggplot(gapminderMinusKuwait, aes(lifeExp, gdpPercap)) +
 dev.off()
 
 #Plot2
-#Giving me a headache, seems impossible to figure out.
-#Tried to cheat but seems no one figured it out.Sighs.
-#The error occurs on adding gapminder_continent to the geom
+
 gapminder_continent <- gapminderMinusKuwait %>%
   group_by(continent, year) %>%
-  summarize(gdpPercapweighted = weighted.mean(x = gdpPercap, w = pop),
+  summarize(gdpPercapweighted = 
+              weighted.mean(x = gdpPercap, w = pop),
             pop = sum(as.numeric(pop)))  
 
-ggplot(gapminderMinusKuwait,aes(x=year, y=gdpPercap)) + 
-  geom_point(aes(size=pop/100000, color=continent)) + 
-  geom_line(aes(color=continent, group=country)) +
+png(filename = "Plot2.png", height=5, width = 15, 
+    units ="in", res=300)
+ggplot(gapminderMinusKuwait,
+       aes(x=year, y=gdpPercap, 
+           color=continent, group=country))+
+  
+  geom_point(aes(size=pop/100000)) + 
+  geom_line() +
+  
+  geom_point(data=gapminder_continent,
+             aes(x=year, y=gdpPercapweighted, size=pop/100000),
+             inherit.aes=FALSE) + 
+  
+  
+  geom_line(data=gapminder_continent,
+            aes(x=year, y=gdpPercapweighted), 
+            inherit.aes=FALSE) + 
+  
   facet_wrap(~continent, nrow = 1) + theme_bw() +
-  labs(x="year", y= "GDP Per Capita", size="Population(100k)") +
-  expand_limits(y=0) +
-  geom_line(gapminder_continent,
-            aes(year, gdpPercapweighted, color="black", inherit.aes=FALSE)) + 
-  geom_point(gapminder_continent,
-             aes(year, gdpPercapweighted, color="black", inherit.aes=FALSE)) 
+  labs(x="year", y= "GDP Per Capita", 
+       size="Population(100k)") 
+dev.off()
+
 
 
 
